3477a710a8a0c8b675f4ab6e6089094159433035
Oğulcan TURAN <ismetogulcanturan@gmail.com>
2025-05-02 02:51:14 +0200

    sample project also included in this repo

---
+++ samples/OpenSettings.Api/MyFirstSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace OpenSettings.Api
+{
+    public class MyFirstSettings
+    {
+        public string Name { get; set; } = "Open";
+
+        public string Description { get; set; }
+    }
+}
